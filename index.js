75528b549dc6aa8535f7f129e6b190055fcf351c
CodingOver <paypal1business.over@gmail.com>
2023-02-09 20:37:11 +0100

    Add Pacman with movement

--- index.js
+++ index.js
@@ -14,10 +14,62 @@ class Boundary {
         this.height = 40
     }
     draw() {
+        // Draw a rectangle using canvas
         ctx.fillStyle = "blue"
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
+
+// Create a Pacman Player
+class Player {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15
+    }
+    draw() {
+        // Draw a circle using canvas
+        ctx.beginPath()
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        ctx.fillStyle = "yellow"
+        ctx.fill()
+        ctx.closePath()
+    }
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+const boundaries = []
+const player = new Player({
+    position: {
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const keys = {
+    top: {
+        pressed: false
+    },
+    right: {
+        pressed: false
+    },
+    bottom: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
+
+let lastKey;
 
 const map = [
     ['-', '-', '-', '-', '-', '-'],
@@ -26,7 +78,6 @@ const map = [
     ['-', ' ', ' ', ' ', ' ', '-'],
     ['-', '-', '-', '-', '-', '-'],
 ]
-const boundaries = []
 
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
@@ -43,6 +94,73 @@ map.forEach((row, i) => {
     })
 })
 
-boundaries.forEach((boundary) => {
-    boundary.draw()
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    boundaries.forEach((boundary) => {
+        boundary.draw()
+    })
+
+    player.update()
+    player.velocity.y = 0
+    player.velocity.x = 0
+
+    if (keys.top.pressed && lastKey === 38) {
+        player.velocity.y = -5
+    } else if (keys.right.pressed && lastKey === 39) {
+        player.velocity.x = 5
+    } else if (keys.bottom.pressed && lastKey === 40) {
+        player.velocity.y = 5
+    } else if (keys.left.pressed && lastKey === 37) {
+        player.velocity.x = -5
+    }
+}
+
+animate()
+
+
+addEventListener("keydown", ({ keyCode }) => {
+    switch (keyCode) {
+        // Top
+        case 38:
+            keys.top.pressed = true
+            lastKey = 38
+            break;
+        // Right
+        case 39:
+            keys.right.pressed = true
+            lastKey = 39
+            break;
+        // Bottom
+        case 40:
+            keys.bottom.pressed = true
+            lastKey = 40
+            break;
+        // Left
+        case 37:
+            keys.left.pressed = true
+            lastKey = 37
+            break;
+    }
+})
+addEventListener("keyup", ({ keyCode }) => {
+    switch (keyCode) {
+        // Top
+        case 38:
+            keys.top.pressed = false
+            break;
+        // Right
+        case 39:
+            keys.right.pressed = false
+            break;
+        // Bottom
+        case 40:
+            keys.bottom.pressed = false
+            break;
+        // Left
+        case 37:
+            keys.left.pressed = false
+            break;
+
+    }
 })
