f160a2c8fdb6e04fb5a321a81c82b53e93fd58ac
CodingOver <paypal1business.over@gmail.com>
2023-02-11 19:10:41 +0100

    Finish The game

--- pacman.js
+++ pacman.js
@@ -1,107 +1,162 @@
-// Create Pacman Game Player
 class Pacman {
     constructor(x, y, width, height, speed) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.speed = speed
-        this.direction = DIRECTION_RIGHT
-        this.currentFrame = 1
-        this.frameCount = 7
-
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = 4;
+        this.nextDirection = 4;
+        this.frameCount = 7;
+        this.currentFrame = 1;
         setInterval(() => {
-            this.changeAnimations()
-
-        }, 100)
+            this.changeAnimation();
+        }, 100);
     }
 
     moveProcess() {
-        this.changeDirectionifPossible()
-        this.moveForward()
-        if (this.checkCollision()) {
-            this.moveBackwords()
+        this.changeDirectionIfPossible();
+        this.moveForwards();
+        if (this.checkCollisions()) {
+            this.moveBackwards();
+            return;
         }
     }
 
     eat() {
-
+        for (let i = 0; i < map.length; i++) {
+            for (let j = 0; j < map[0].length; j++) {
+                if (
+                    map[i][j] == 2 &&
+                    this.getMapX() == j &&
+                    this.getMapY() == i
+                ) {
+                    map[i][j] = 3;
+                    score++;
+                }
+            }
+        }
     }
 
-    moveBackwords() {
+    moveBackwards() {
         switch (this.direction) {
-            case DIRECTION_RIGHT:
-                this.x -= this.speed
+            case DIRECTION_RIGHT: // Right
+                this.x -= this.speed;
                 break;
-            case DIRECTION_UP:
-                this.y += this.speed
+            case DIRECTION_UP: // Up
+                this.y += this.speed;
                 break;
-            case DIRECTION_LEFT:
-                this.x += this.speed
+            case DIRECTION_LEFT: // Left
+                this.x += this.speed;
                 break;
-            case DIRECTION_BOTTOM:
-                this.y -= this.speed
+            case DIRECTION_BOTTOM: // Bottom
+                this.y -= this.speed;
                 break;
         }
     }
 
-    moveForward() {
+    moveForwards() {
         switch (this.direction) {
-            case DIRECTION_RIGHT:
-                this.x += this.speed
+            case DIRECTION_RIGHT: // Right
+                this.x += this.speed;
                 break;
-            case DIRECTION_UP:
-                this.y -= this.speed
+            case DIRECTION_UP: // Up
+                this.y -= this.speed;
                 break;
-            case DIRECTION_LEFT:
-                this.x -= this.speed
+            case DIRECTION_LEFT: // Left
+                this.x -= this.speed;
                 break;
-            case DIRECTION_BOTTOM:
-                this.y += this.speed
+            case DIRECTION_BOTTOM: // Bottom
+                this.y += this.speed;
                 break;
         }
     }
 
-    checkCollision() {
-        let isCollided = false
+    checkCollisions() {
+        let isCollided = false;
         if (
-            map[this.getMapY()][this.getMapX()] == 1 ||
-            map[this.getMapYRightSide()][this.getMapX()] == 1 ||
-            map[this.getMapY()][this.getMapXRightSide()] == 1 ||
-            map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
+            map[parseInt(this.y / oneBlockSize)][
+            parseInt(this.x / oneBlockSize)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize + 0.9999)][
+            parseInt(this.x / oneBlockSize)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize)][
+            parseInt(this.x / oneBlockSize + 0.9999)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize + 0.9999)][
+            parseInt(this.x / oneBlockSize + 0.9999)
+            ] == 1
         ) {
-            return true
+            isCollided = true;
         }
-        return false
+        return isCollided;
     }
 
-    checkGhostCollision() {
-
+    checkGhostCollision(ghosts) {
+        for (let i = 0; i < ghosts.length; i++) {
+            let ghost = ghosts[i];
+            if (
+                ghost.getMapX() == this.getMapX() &&
+                ghost.getMapY() == this.getMapY()
+            ) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    changeDirectionifPossible() {
-
+    changeDirectionIfPossible() {
+        if (this.direction == this.nextDirection) return;
+        let tempDirection = this.direction;
+        this.direction = this.nextDirection;
+        this.moveForwards();
+        if (this.checkCollisions()) {
+            this.moveBackwards();
+            this.direction = tempDirection;
+        } else {
+            this.moveBackwards();
+        }
     }
 
-    changeAnimations() {
-        this.currentFrame = this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1
+    getMapX() {
+        let mapX = parseInt(this.x / oneBlockSize);
+        return mapX;
+    }
+
+    getMapY() {
+        let mapY = parseInt(this.y / oneBlockSize);
+
+        return mapY;
+    }
+
+    getMapXRightSide() {
+        let mapX = parseInt((this.x * 0.99 + oneBlockSize) / oneBlockSize);
+        return mapX;
+    }
+
+    getMapYRightSide() {
+        let mapY = parseInt((this.y * 0.99 + oneBlockSize) / oneBlockSize);
+        return mapY;
+    }
+
+    changeAnimation() {
+        this.currentFrame =
+            this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
     }
 
     draw() {
-        ctx.save()
-        ctx.translate(
+        canvasContext.save();
+        canvasContext.translate(
             this.x + oneBlockSize / 2,
             this.y + oneBlockSize / 2
-        )
-
-        ctx.rotate((this.direction * 90 * Math.PI) / 180)
-
-        ctx.translate(
+        );
+        canvasContext.rotate((this.direction * 90 * Math.PI) / 180);
+        canvasContext.translate(
             -this.x - oneBlockSize / 2,
             -this.y - oneBlockSize / 2
-        )
-
-        ctx.drawImage(
+        );
+        canvasContext.drawImage(
             pacmanFrames,
             (this.currentFrame - 1) * oneBlockSize,
             0,
@@ -111,25 +166,7 @@ class Pacman {
             this.y,
             this.width,
             this.height
-        )
-
-        ctx.restore()
-    }
-
-    getMapX() {
-        return parseInt(this.x / oneBlockSize)
-    }
-
-    getMapY() {
-        return parseInt(this.y / oneBlockSize)
-    }
-
-    getMapXRightSide() {
-        return parseInt((this.x + 0.9999 * oneBlockSize) / oneBlockSize)
-    }
-
-    getMapYRightSide() {
-        return parseInt((this.y + 0.9999 * oneBlockSize) / oneBlockSize)
+        );
+        canvasContext.restore();
     }
 }
-
